eae9ed95f1947288a726724eaf00ced3bc47b502
kamran <kamcoder94@gmail.com>
2020-09-05 04:31:21 +0100

    static files removed

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
